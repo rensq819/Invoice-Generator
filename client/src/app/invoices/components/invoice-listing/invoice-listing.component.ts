98a26bab85b70fd4e1e346a822d6ec1f538983cc
Siqi Ren <sren@unitedsolutions.local>
2019-04-23 14:17:39 -0400

    running observables in series

--- client/src/app/invoices/components/invoice-listing/invoice-listing.component.ts
+++ client/src/app/invoices/components/invoice-listing/invoice-listing.component.ts
@@ -1,52 +1,68 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { InvoiceService } from "../../services/invoice.service";
-import { Invoice } from "../../models/invoice";
-import { Router } from "@angular/router";
-import { MatSnackBar, MatPaginator } from "@angular/material";
-import { remove } from "lodash";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { InvoiceService } from '../../services/invoice.service';
+import { Invoice } from '../../models/invoice';
+import { Router } from '@angular/router';
+import { MatSnackBar, MatPaginator } from '@angular/material';
+import { remove } from 'lodash';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
-  selector: "app-invoice-listing",
-  templateUrl: "./invoice-listing.component.html",
-  styleUrls: ["./invoice-listing.component.scss"]
+  selector: 'app-invoice-listing',
+  templateUrl: './invoice-listing.component.html',
+  styleUrls: ['./invoice-listing.component.scss']
 })
 export class InvoiceListingComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  displayedColumns = ["item", "date", "due", "qty", "rate", "tax", "action"];
+  displayedColumns = ['item', 'date', 'due', 'qty', 'rate', 'tax', 'action'];
   dataSource: Invoice[] = [];
   resultsLength = 0;
 
-  constructor(
-    private invoiceService: InvoiceService,
-    private router: Router,
-    private snackBar: MatSnackBar
-  ) {}
+  constructor(private invoiceService: InvoiceService, private router: Router, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
-    this.paginator.page.subscribe(
-      data => {
-        this.invoiceService
-          .getInvoices({
-            page: ++data.pageIndex,
+    this.paginator.page
+      .pipe(
+        mergeMap(data => {
+          return this.invoiceService.getInvoices({
+            page: data.pageIndex + 1,
             perPage: data.pageSize
-          })
-          .subscribe(data => {
-            console.log(data);
-            this.dataSource = data.docs;
-            this.resultsLength = data.total;
           });
-      },
-      err => this.errorHandler(err, "Failed to delete invoice")
-    );
+        })
+      )
+      .subscribe(
+        data => {
+          this.dataSource = data.docs;
+          this.resultsLength = data.total;
+        },
+        err => {
+          this.errorHandler(err, 'Failed to delete invoice');
+        }
+      );
+
+    // this.paginator.page.subscribe(
+    //   data => {
+    //     this.invoiceService
+    //       .getInvoices({
+    //         page: ++data.pageIndex,
+    //         perPage: data.pageSize
+    //       })
+    //       .subscribe(data => {
+    //         console.log(data);
+    //         this.dataSource = data.docs;
+    //         this.resultsLength = data.total;
+    //       });
+    //   },
+    //   err => this.errorHandler(err, 'Failed to delete invoice')
+    // );
     this.populateInvoices();
   }
 
   createBtnHandler() {
-    this.router.navigate(["dashboard", "invoices", "new"]);
+    this.router.navigate(['dashboard', 'invoices', 'new']);
   }
 
   editBtnHandler(id) {
-    this.router.navigate(["dashboard", "invoices", id]);
+    this.router.navigate(['dashboard', 'invoices', id]);
   }
 
   deleteBtnHandler(id) {
@@ -56,26 +72,24 @@ export class InvoiceListingComponent implements OnInit {
           return item._id === data._id;
         });
         this.dataSource = [...this.dataSource];
-        this.snackBar.open("Invoice deleted", "Success", {
+        this.snackBar.open('Invoice deleted', 'Success', {
           duration: 3000
         });
       },
-      err => this.errorHandler(err, "Failed to delete invoice")
+      err => this.errorHandler(err, 'Failed to delete invoice')
     );
   }
 
   private populateInvoices() {
-    this.invoiceService
-      .getInvoices({ page: 1, perPage: 10 })
-      .subscribe(data => {
-        this.dataSource = data.docs;
-        this.resultsLength = data.total;
-      }),
-      err => this.errorHandler(err, "Failed to fetch invoices");
+    this.invoiceService.getInvoices({ page: 1, perPage: 10 }).subscribe(data => {
+      this.dataSource = data.docs;
+      this.resultsLength = data.total;
+    }),
+      err => this.errorHandler(err, 'Failed to fetch invoices');
   }
   private errorHandler(error, message) {
     console.error(error);
-    this.snackBar.open(message, "Error", {
+    this.snackBar.open(message, 'Error', {
       duration: 3000
     });
   }
