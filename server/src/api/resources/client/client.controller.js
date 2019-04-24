ea9dda2fb5bf5dca1e5cea54af95274ab065c25e
rensq819 <siqi.ren@gmail.com>
2019-04-24 15:51:33 -0400

    create client record using async and await

--- server/src/api/resources/client/client.controller.js
+++ server/src/api/resources/client/client.controller.js
@@ -1,7 +1,19 @@
 import { create } from 'domain';
+import clientService from './client.service';
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from 'http-status-codes';
+import Client from './client.model';
 
 export default {
   async create(req, res) {
-    return res.json({ msg: 'Create Client' });
+    try {
+      const { value, error } = clientService.validateCreateSchema(req.body);
+      if (error && error.details) {
+        return res.status(BAD_REQUEST).json(error);
+      }
+      const client = await Client.create(value);
+      return res.json(client);
+    } catch(error) {
+      res.status(INTERNAL_SERVER_ERROR).json(error);
+    }
   }
 };
---
+++ server/src/api/resources/client/client.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const ClientSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+});
+
+
+export default mongoose.model('Client', ClientSchema);
